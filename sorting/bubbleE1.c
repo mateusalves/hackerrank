c202f5f9f25ce9e8aa8ed95e917208255f5db0f7
Mateus Alves <mateus.alves.unb@gmail.com>
2021-08-18 07:56:31 -0300

    Adding sorting exercise

---
+++ sorting/bubbleE1.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+void countSwaps( int a_count, int* a)
+{
+  int numSwaps = 0;
+  for(int i = 0; i < a_count-1; i++)
+  {
+    for(int j = 0; j < a_count-i-1; j++)
+    {
+      if(*(a + j) > *(a+j+1))
+      {
+        int tmp = *(a + j);
+        *(a + j) = *(a + j + 1);
+        *(a + j + 1) = tmp;
+        numSwaps++;
+      }
+    }
+    if(a_count - i - 1 == 1)
+      break;
+  }
+
+  printf("Array is sorted in %d swaps.\n", numSwaps);
+  printf("First Element: %d\n", *(a));
+  printf("Last Element: %d\n", *(a+a_count-1));
+}
+
+int main(int argc, char **argv){
+  int n = 3;
+
+  int arr[] = {6, 4, 1};
+
+  countSwaps(n, arr);
+
+  printf("Array sorted:\n");
+  for(int k = 0; k < n; k++)
+    printf("%d ", *(arr + k));
+  printf("\n");
+
+}
